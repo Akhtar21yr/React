6bc18dc57ee5b5a9103b2c97f14bb788959c8cc5
akhtar21yr <akhtar.decy@gmail.com>
2024-09-14 17:01:19 +0530

    07-reactRouter Done

--- 07-reactRouter/src/main.jsx
+++ 07-reactRouter/src/main.jsx
@@ -6,6 +6,11 @@ import Layout from './Layout.jsx'
 import './index.css'
 import About from './components/About/About'
 import Home from './components/Home/Home'
+import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
+import Github,{githubInfoLoader} from './components/Github/Github.jsx'
+
+
 
 
 const router = createBrowserRouter([
@@ -21,6 +26,19 @@ const router = createBrowserRouter([
         path : "about",
         element : <About/>
       },
+      {
+        path:"contact",
+        element : <Contact/>
+      },
+      {
+        path:"user/:id",
+        element : <User/>
+      },
+      {
+        path : "github",
+        element : <Github/> ,
+        loader : githubInfoLoader
+      }
 
       
     ]
